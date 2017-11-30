4ef6454f710ed65797a8e309734408d54c8c9733
toezer <timur.oezer@gmail.com>
2017-11-30 14:59:32 +0100

    fix sqlalchemy db connection #1

--- app.py
+++ app.py
@@ -2,18 +2,28 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PeerReviewSystem.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PeerReviewSystem.db'
 
 db = SQLAlchemy(app)
+
+
 class User(db.Model):
-   id = db.Column('user_id', db.Integer, primary_key=True)
-   email = db.Column(db.String(100), unique=True)
-   password = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
 
 @app.route('/')
 def index():
-        return render_template('index.html',users = User.query.all())
+    return render_template('index.html', users=User.query.all())
+
 
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
+    db.session.add(User(email='superChairman@mail.com', password='12345'))
+    db.session.commit()
+
+    print('all users: ', User.query.all())
+
     app.run(debug=True)
- 
