4fff5f271fee10418b4b49210399b33c96d7764a
WendtOle <dielanie@freenet.de>
2017-11-30 14:29:17 +0100

    set .sql file name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PeerReview-System.sqlite3'
 
 @app.route('/')
 def index():
