58acf150769cec3e1b8e1c8dfdd95bdde6ed5a56
WendtOle <dielanie@freenet.de>
2017-11-30 16:23:33 +0100

    add isConferenceChairColumn to module and add to example users

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PeerReviewSystem.db'
 
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-
+    isConferenceChair = db.Column(db.BOOLEAN)
 
 @app.route('/')
 def index():
@@ -21,7 +20,8 @@ def index():
 if __name__ == '__main__':
     db.drop_all()
     db.create_all()
-    db.session.add(User(email='superChairman@mail.com', password='12345'))
+    db.session.add(User(email='superChairman@mail.com', password='12345',isConferenceChair = True))
+    db.session.add(User(email='ole-wendt@freenet.de', password='forget', isConferenceChair=False))
     db.session.commit()
 
     print('all users: ', User.query.all())
