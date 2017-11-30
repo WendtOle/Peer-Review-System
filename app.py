92d7ffeb4a12d4d62531de3bda23cfc0fc5aa93d
WendtOle <dielanie@freenet.de>
2017-11-30 14:36:46 +0100

    create class User with fields

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PeerReview-System.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PeerReviewSystem.sqlite3'
+
+db = SQLAlchemy(app)
+class User(db.Model):
+   id = db.Column('user_id', db.Integer, primary_key=True)
+   email = db.Column(db.String(100), unique=True)
+   password = db.Column(db.String(100))
 
 @app.route('/')
 def index():
