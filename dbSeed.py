e17bc229605a21601d7512d245f17e5d9653af80
WendtOle <dielanie@freenet.de>
2017-12-04 15:50:44 +0100

    added paper to seed file

--- dbSeed.py
+++ dbSeed.py
@@ -6,8 +6,10 @@ def init():
     app.db.drop_all()
     app.db.create_all()
     session = app.db.session()
-    session.add(models.User(email='superChairman@mail.com', password='12345', isConferenceChair=True))
+    user01 = models.User(email='superChairman@mail.com', password='12345', isConferenceChair=True)
+    session.add(user01)
     session.add(models.User(email='ole-wendt@freenet.de', password='forget', isConferenceChair=False))
+    session.add(models.Paper(title="Fantastic Paper over 9000", abstract="Abstract about this Paper", authors=[user01]))
     session.commit()
 
     print('all users: ', models.User.query.all())
